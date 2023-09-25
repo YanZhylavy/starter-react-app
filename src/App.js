86c25e1b8f4a3849606feae040fd435e5f3e3d34
YanZhylavy <127868644+YanZhylavy@users.noreply.github.com>
2023-09-25 20:42:34 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is YanZhylavy's example REACT website :)
+          This is Yan Zhylavy's example REACT website :)
         </p>
         <a
           className="App-link"
